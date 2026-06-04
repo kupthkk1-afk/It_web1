1aad24c3aaa177b3d0ca3fb69f6d657a11d06b43
kupthkk1-afk <kupth.kk1@gmail.com>
2026-06-04 09:26:09 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div >
+        <h1>Welcome to our Wevb</h1>
+        <p>By Kupth Kongklai</p>  
+    </div>
+  );
+}
